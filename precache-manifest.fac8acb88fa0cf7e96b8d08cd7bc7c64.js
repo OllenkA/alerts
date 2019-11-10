a50f57ca3531387987f67c205564983ab9b3c0d3
OllenkA <shamko.olga2018@gmail.com>
2019-11-10 22:32:48 +0300

    Updates

---
+++ precache-manifest.fac8acb88fa0cf7e96b8d08cd7bc7c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cb38a5294e27ad89f7df414127a2ef",
+    "url": "/alerts/index.html"
+  },
+  {
+    "revision": "9b9b3119b2679476815d",
+    "url": "/alerts/static/css/main.014c0f6f.chunk.css"
+  },
+  {
+    "revision": "1a033c6b302e8be991e6",
+    "url": "/alerts/static/js/2.5e80a538.chunk.js"
+  },
+  {
+    "revision": "9b9b3119b2679476815d",
+    "url": "/alerts/static/js/main.b7b94fa5.chunk.js"
+  },
+  {
+    "revision": "9fc84b541b24f70ecbbf",
+    "url": "/alerts/static/js/runtime-main.93eb65e7.js"
+  }
+]);
