ab1c1c06718e3845b63ea0099dc6b6e1c0ce2b9b
OllenkA <shamko.olga2018@gmail.com>
2019-11-10 22:01:57 +0300

    Test task, full description in README.md

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './../App.css';
+import {connect} from "react-redux";
+import {showWindow} from "../redux/reducer";
+
+
+const MainContent = (props) => {
+    let addWindow = (styles, title) => {
+        return props.showWindow(styles, title)
+    };
+
+    return <main className="container">
+        <div className='main'>
+            {props.alerts.map((al, i) =>
+                (<button key={i} className='buttons' style={al.styles}
+                         onClick={() => {addWindow(al.styles, al.title)}}>
+                    {al.title}
+                </button>))}
+        </div>
+
+        <div className='popUp'>
+            {props.popUpAlerts.length >= 1 ?
+                props.popUpAlerts.map((pop, i) => {
+                    return <div key={i} className='pop' style={pop.styles}>
+                        {pop.title}
+                    </div>})
+                :null}
+        </div>
+
+    </main>
+};
+
+const mapStateToProps = (state) => ({
+    alerts: state.main.alerts,
+    popUpAlerts: state.main.popUpAlerts,
+});
+
+export default connect(mapStateToProps, {showWindow})(MainContent);
---
+++ src/redux/reducer.js
@@ -0,0 +1,54 @@
+const ADD_WINDOW = 'ADD_WINDOW';
+const CLEAR_WINDOW = 'CLEAR_WINDOW';
+
+
+const initialState = {
+    alerts: [
+        {id: 1, title: 'Success', styles: {border: '2px solid green'}},
+        {id: 2, title: 'Warning', styles: {border: '2px solid yellow'}},
+        {id: 3, title: 'Error', styles: {border: '2px solid red'}}
+    ],
+    popUpAlerts: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_WINDOW: {
+            return {
+                ...state,
+                popUpAlerts: [
+                    ...state.popUpAlerts,
+                    action.object
+                ]
+            }
+        }
+        case CLEAR_WINDOW: {
+            let newArray = state.popUpAlerts.filter(pop => pop.id !== action.id);
+            return {
+                ...state,
+                popUpAlerts: newArray
+            }
+        }
+        default:
+            return state;
+    }
+};
+
+export const showWindow = (styles, title) => (dispatch) => {
+    const data = {
+        id: new Date().valueOf(),
+        styles,
+        title
+    };
+    dispatch(addWindow(data));
+    debugger
+    setTimeout(() => {
+        dispatch(clearWindow(data.id))
+    }, 3000)
+};
+
+export const addWindow = (object) => ({type: ADD_WINDOW, object});
+export const clearWindow = (id) => ({type: CLEAR_WINDOW, id});
+
+
+export default reducer;
